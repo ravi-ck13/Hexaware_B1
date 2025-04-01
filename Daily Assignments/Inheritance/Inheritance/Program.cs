37b10a3d733e007fd095be137501231d4630b22b
ravi-ck13 <ravikumarck13@gmail.com>
2025-04-01 09:59:31 +0530

    Assignments 1 and 2

--- Daily Assignments/Inheritance/Inheritance/Program.cs
+++ Daily Assignments/Inheritance/Inheritance/Program.cs
@@ -4,9 +4,9 @@
     {
         static void Main(string[] args)
         {
-            Employee employee = new Employee(101, "Rajesh", "2003-05-28", 45000);
-            Manager manager = new Manager(100, "John", "1990-04-01", 50000, 5000, 10000);
-            Employee mgr = new Manager(100, "John", "1990-04-01", 60000, 7000, 6000);
+            Employee employee = new Employee(101, "Raj", "2003-05-28", 45000);
+            Manager manager = new Manager(100, "Ravi", "1990-04-01", 50000, 5000, 10000);
+            Employee mgr = new Manager(100, "Hari", "1990-04-01", 60000, 7000, 6000);
 
             Console.WriteLine(employee.ComputeSalary());
             Console.WriteLine(manager.ComputeSalary());
